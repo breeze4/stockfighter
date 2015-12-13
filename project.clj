7cc22f0698a97968c94255908c81c98b795d6f0b
breeze4 <ambiturner0@gmail.com>
2015-12-13 10:05:41 -0800

    finished level 1, not perfect score yet

--- project.clj
+++ project.clj
@@ -2,5 +2,9 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
+                 [environ "1.0.0"]
+                 [org.clojure/data.json "0.2.6"]]
+  :plugins [[lein-environ "1.0.0"]])
--- src/stockfighter/level_1.clj
+++ src/stockfighter/level_1.clj
@@ -1,1 +1,52 @@
-(ns stockfighter.level-1)
+(ns stockfighter.level-1
+  (:require [clj-http.client :as http]
+            [environ.core :refer [env]]
+            [clojure.data.json :as json]))
+
+(def api-key (env :stockfighter-key))
+(def auth-header {"X-Starfighter-Authorization" api-key})
+(def gm-url "https://www.stockfighter.io/gm")
+(def trade-url "https://www.stockfighter.io/ob/api")
+(def level-one "/levels/first_steps")
+
+; test data
+;(def exchange "TESTEX")
+;(def stock "FOOBAR")
+;(def account "EXB123456")
+
+(def state (atom {:instanceId nil
+                  :venue      nil
+                  :ticker     nil
+                  :account    nil}))
+
+(defn parse-body [response]
+  (json/read-str (:body response) :key-fn keyword))
+
+(defn order [direction quantity stock venue account type]
+  (let [response (http/post (str trade-url "/venues/" venue "/stocks/" stock "/orders")
+                            {:headers      auth-header
+                             :content-type :json
+                             :body         (json/write-str {:account   account
+                                                            :venue     venue
+                                                            :stock     stock
+                                                            :qty       quantity
+                                                            :direction direction
+                                                            :orderType type})})
+        body (parse-body response)]
+    body))
+
+(defn init-level []
+  (let [response (http/post (str gm-url level-one) {:headers auth-header})
+        body (parse-body response)
+        instance (swap! state assoc :instanceId (body :instanceId))
+        account (swap! state assoc :account (body :account))
+        ticker (swap! state assoc :ticker (first (body :tickers)))
+        venue (swap! state assoc :ticker (first (body :venues)))]
+    (if (body :ok)
+      (prn "success! ready" instance account ticker venue)
+      ;(order "buy" 100 ticker venue account "market")
+      (prn "error" response)
+      )
+    ))
+
+;;
